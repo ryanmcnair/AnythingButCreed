154d7b7e93d7ce3bcd67f1acd0b9ac72385a69e0
Ryan McNair <ryan.mcnair@gmail.com>
2021-02-04 20:58:08 -0600

    finished exercise

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnythingButCreed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var goodSongs = new List<Song>();
+
+            var allSongs = new List<Song>()
+            {
+                new Song("The Beatles", "Get Back" ),
+                new Song("Led Zeppelin", "When the levee breaks"),
+                new Song("The Smashing Pumpkins", "1979"),
+                new Song("Slayer", "Dead skin mask"),
+                new Song("Hall & Oates", "Maneater"),
+                new Song("Creed", "Take me higher")
+            };
+
+            goodSongs.AddRange(allSongs.Where(song => song.Artist != "Creed"));
+
+            foreach (var song in goodSongs)
+            {
+                Console.WriteLine($"{song.Artist} {song.Name}");
+            }
+
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnythingButCreed
+{
+    class Song
+    {
+        public string Artist { get; set; }
+        public string Name { get; set; }
+
+        public Song(string artist, string name)
+        {
+            Artist = artist;
+            Name = name;
+        }
+    }
+}
